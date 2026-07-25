4857b1d6f920ba79889fd005fffc6546a42da87d
info.satyam.dev@gmail.com <info.satyam.dev@gmail.com>
2026-07-26 00:00:12 +0530

    feat: enhanced version of for loops

---
+++ enhanced_application_of_for_loops.java
@@ -0,0 +1,8 @@
+public class enhanced_application_of_for_loops {
+    public static void main (String args[]){
+        int arr[] = {1, 2, 3, 4, 5};
+        for( int n : arr){
+            System.out.println(n);
+        }
+    }
+}
