585f1ba693d5a033314fc0537fbdbbc94c7fa9f2
info.satyam.dev@gmail.com <info.satyam.dev@gmail.com>
2026-07-31 23:05:24 +0530

    enum class

--- EnumClass.java
+++ EnumClass.java
@@ -1,17 +1,28 @@
-enum Status{
-    Running, Failed, Pending, Success, Aborted; 
+enum Laptop{
+    Mac(120000), Samsung(80000), Lenovo(50000), Dell(60000), MSI;
+    private int price;
+
+    // this default constructor will set all the missing values of objects with no price set as its default value and constructor will be called only when no value is assigned
+    private Laptop(){
+        price =50000;
+        System.out.println("Default constructor called");
+    }
+    private Laptop(int price){
+        this.price = price;
+        System.out.println("Parameterized constructor called");
+    }
+    public int getPrice(){
+        return price;
+    }
+    public void setPrice(int price){
+        this.price = price;
+    }
 }
 
 public class EnumClass {
-    public static void main(String args[]){
-        Status s = Status.Aborted;
-        System.out.println(s);
-        Status[] st = Status.values();
-        for(int i = 0; i<st.length; i++){
-            System.out.print(st[i] + ",    ");
-        }
-        for(Status ss :st){
-            System.out.println(ss + ":" + ss.ordinal());
+    public static void main(String[] args) {
+        for(Laptop lap: Laptop.values()){
+            System.out.println(lap + " : " + lap.getPrice());
         }
     }
 }
