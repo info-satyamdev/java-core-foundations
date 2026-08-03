350f79fa2d4db7042e8e9fd5b7db3c84c7206653
info.satyam.dev@gmail.com <info.satyam.dev@gmail.com>
2026-08-03 20:25:42 +0530

    ArrayLists Collection in java

--- ArrayListsa.java
+++ ArrayListsa.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 import java.util.Collection;
-import javax.xml.transform.Source;
+
 
 public class ArrayListsa{
     public static void main(String[] args) {
@@ -10,10 +10,15 @@ public class ArrayListsa{
         num.add(4);
         num.add(7);
         num.add(9);
-
+        num.add(5);
+        num.add(4);
+        num.add(7);
+        num.add(9);
+        System.out.println("Arraylist:");
         for(Object n :num){
             int nums = (Integer)n;
             System.out.println(nums);
         }
+        
     }    
 }
